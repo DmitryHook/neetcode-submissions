93f2d3395e7f4b809d4530272ee843f35de56888
Dmitry <76666284+DmitryHook@users.noreply.github.com>
2026-08-16 15:28:56 +0300

    Add: minimum-number-of-operations-to-move-all-balls-to-each-box - submission-0

---
+++ Data Structures & Algorithms/minimum-number-of-operations-to-move-all-balls-to-each-box/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        n = len(boxes)
+        result = [0] * n
+
+        balls = 0
+        ops = 0
+
+        for i in range(n):
+            result[i] += ops
+
+            if boxes[i] == '1':
+                balls += 1
+            
+            ops += balls
+        
+        balls = 0
+        ops = 0
+
+        for i in range(n-1, -1, -1):
+            result[i] += ops
+
+            if boxes[i] == '1':
+                balls += 1
+            
+            ops += balls
+
+        return result
