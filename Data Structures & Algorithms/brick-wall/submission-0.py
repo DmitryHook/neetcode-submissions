10d4136e30cd7320a3484b92746be6addfe436c1
Dmitry <76666284+DmitryHook@users.noreply.github.com>
2026-08-18 20:31:35 +0300

    Add: brick-wall - submission-0

---
+++ Data Structures & Algorithms/brick-wall/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        edges = defaultdict(int)
+
+        for row in wall:
+            pos = 0
+
+            for brick in row[:-1]:
+                pos += brick
+                edges[pos] += 1
+
+        max_edges = max(edges.values(), default=0)
+
+        return len(wall) - max_edges
