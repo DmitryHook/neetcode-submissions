36c45749005b65401d65ea96bde034223fd77531
Dmitry <76666284+DmitryHook@users.noreply.github.com>
2026-09-13 15:01:08 +0300

    Add: minimum-number-of-operations-to-make-array-empty - submission-0

---
+++ Data Structures & Algorithms/minimum-number-of-operations-to-make-array-empty/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        freq = Counter(nums)
+        ops = 0
+        
+        for count in freq.values():
+            if count == 1:
+                return -1
+
+            ops += count // 3
+
+            if count % 3 != 0:
+                ops += 1
+
+        return ops
